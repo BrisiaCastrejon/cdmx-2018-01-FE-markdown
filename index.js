ef4ccdd980a3a9acb2ce87a2317d9b22865622d7
BrisiaCastrejon <brissmonse@gmail.com>
2018-08-31 13:56:50 -0500

    Primera funcion: lee el documento

---
+++ index.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const readmeMd = './README.md';
+const path = require('path');
+
+    fs.readFile(readmeMd, 'utf-8', Callcallback = (err, md)=>{      
+        if (err) {
+            console.log(err.message); 
+        }else {
+            const search = /((\bhttps?:\/\/)|(\bhttp:\/\/)|(\bwww\.))\S*/g;
+            const links = md.match(search);   
+         for (let i = 0; i < links.length; i++) {
+            const cutLink = links[i].split(')');
+            const result = cutLink[0];
+            console.log(result);             
+         } /* for (const key in links) {
+             const element = links[key];
+             console.log(element)
+            } */}
+    }) 
---
+++ test/md-links.spec.js
@@ -0,0 +1,9 @@
+const {mdLinks} = require('./../index');
+describe('mdLinks, recorre un documento y devuelve las url', () =>{
+  test('mdLinks, debe ser una funcion', () => {
+    expect(mdLinks).toEqual('function')
+  })
+});
+
+
+ 
