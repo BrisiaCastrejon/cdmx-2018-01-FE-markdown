b2982f0f60e31c4947d8bf501f8e11643a59f120
BrisiaCastrejon <brissmonse@gmail.com>
2018-09-03 11:53:14 -0500

    Refactorización de funciones

--- index.js
+++ index.js
@@ -1,19 +1,61 @@
+#!/usr/bin/env node
+
 const fs = require("fs");
 const readmeMd = './README.md';
 const path = require('path');
 
-    fs.readFile(readmeMd, 'utf-8', Callcallback = (err, md)=>{      
-        if (err) {
-            console.log(err.message); 
-        }else {
-            const search = /((\bhttps?:\/\/)|(\bhttp:\/\/)|(\bwww\.))\S*/g;
-            const links = md.match(search);   
-         for (let i = 0; i < links.length; i++) {
+/* const getLinks = (err, lines) => {
+    if (err) {
+        console.log(err.message);
+    } else {
+        console.log(lines);
+
+    }
+}; */
+
+/* const getLines = (err, md) => {
+    if (err) {
+        console.log(err.message);
+    } else {
+        const lines = md.split('\n');
+        // console.log(lines);
+       //getLinks(err, lines);
+    };
+};*/
+const getLinks = (err, md) => {
+    if (err) {
+        console.log(err.message);
+    } else {
+        const search = /((\bhttps?:\/\/)|(\bhttp:\/\/)|(\bwww\.))\S*/g;
+        const links = md.match(search);
+        for (let i = 0; i < links.length; i++) {
             const cutLink = links[i].split(')');
             const result = cutLink[0];
-            console.log(result);             
-         } /* for (const key in links) {
-             const element = links[key];
-             console.log(element)
-            } */}
-    }) 
+            console.log(result);
+        }
+    }
+};
+
+const getLines = (err, md) => {
+    if (err) {
+        console.log(err.message);
+    } else {
+        const lines = md.split('\n').length;
+        console.log(lines);
+        //getLinks(err, lines);
+    };
+};
+
+const readDocument = (readmeMd, getLines, getLinks) => {
+    fs.readFile(readmeMd, 'utf-8', getFile = (err, md) => {
+        if (err) {
+            console.log(err.message);
+        } else {
+            getLines(err, md);
+            getLinks(err, md);
+        };
+    });
+};
+readDocument(readmeMd, getLines, getLinks);
+
+module.exports = {readDocument};
